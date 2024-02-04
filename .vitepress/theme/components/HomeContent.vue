116078f3eec97439dc41013e77b3f83877284fec
hk <Xiaojin@1017>
2024-02-04 14:21:47 +0800

    修改首页图片路径

--- .vitepress/theme/components/HomeContent.vue
+++ .vitepress/theme/components/HomeContent.vue
@@ -33,7 +33,7 @@ const cardImgStyle = (width, height) => {
                     style="width: 100%;height: 100%;overflow: hidden;">
                     <template #cover>
                         <img :style="cardImgStyle(item.w, item.h)" alt="example"
-                            :src="'/assets/img/' + item?.imgName" />
+                            :src="'/knowledge-graph/assets/img/' + item?.imgName" />
                     </template>
                     <a-card-meta :title="item.name">
                         <template #description>
