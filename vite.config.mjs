1b1fc30c3ecd0a4cf51608727c1b7acfb76a1cd1
hk <Xiaojin@1017>
2024-02-04 11:02:04 +0800

    删除自动按需导入

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,26 +1,25 @@
 import { defineConfig } from 'vite';
-import Components from 'unplugin-vue-components/vite';
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 export default defineConfig({
   plugins: [
     // ...
-    Components({
-      resolvers: [
-        AntDesignVueResolver({
-          importStyle: false, // css in js
-        }),
-      ],
-    }),
   ],
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks(id) {
-  //         if (id.includes('node_modules')) {
-  //           return id.toString().split('node_modules/')[1].split('/')[0].toString();
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if(id.toString().split('node_modules/')[1].split('/')[0].includes("ant-design-vue")) {
+              return "ant-design-vue";
+
+            } else {
+              return "wq";
+            }
+            // console.log("----", id.toString().split('node_modules/')[1].split('/')[0].toString());
+            // return "11111";
+          }
+        }
+      }
+    }
+  }
 });
