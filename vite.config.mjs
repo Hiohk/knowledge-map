da6368c1c6b9b581851926d3c6eca72f5e17551f
hk <Xiaojin@1017>
2024-02-04 11:14:46 +0800

    修改vite.config

--- vite.config.mjs
+++ vite.config.mjs
@@ -9,14 +9,7 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if(id.toString().split('node_modules/')[1].split('/')[0].includes("ant-design-vue")) {
-              return "ant-design-vue";
-
-            } else {
-              return "wq";
-            }
-            // console.log("----", id.toString().split('node_modules/')[1].split('/')[0].toString());
-            // return "11111";
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
         }
       }
