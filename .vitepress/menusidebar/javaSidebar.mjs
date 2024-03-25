eec1aeece59162ee822ae3afae5dddabd376f858
hk <Xiaojin@1017>
2024-03-25 17:47:09 +0800

    第四章数组

--- .vitepress/menusidebar/javaSidebar.mjs
+++ .vitepress/menusidebar/javaSidebar.mjs
@@ -16,5 +16,45 @@ export default [
     {
         text: '数组',
         link: `${commonPath}/java04`
+    },
+    {
+        text: '异常',
+        link: `${commonPath}/java05`
+    },
+    {
+        text: '常用类',
+        link: `${commonPath}/java06`
+    },
+    {
+        text: '集合框架',
+        link: `${commonPath}/java07`
+    },
+    {
+        text: 'IO 流',
+        link: `${commonPath}/java08`
+    },
+    {
+        text: '多线程',
+        link: `${commonPath}/java09`
+    },
+    {
+        text: '反射机制与注解',
+        link: `${commonPath}/java10`
+    },
+    {
+        text: '网络编程',
+        link: `${commonPath}/java11`
+    },
+    {
+        text: 'Lambda 表达式',
+        link: `${commonPath}/java12`
+    },
+    {
+        text: 'Stream 流',
+        link: `${commonPath}/java13`
+    },
+    {
+        text: 'Java8 ~ Java21 重要新特性',
+        link: `${commonPath}/java14`
     }
 ]
