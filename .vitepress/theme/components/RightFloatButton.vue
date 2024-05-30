bb5d36ba8cc5791f36a3535066252955d571cdd1
hk <Xiaojin@1017>
2024-05-30 15:56:00 +0800

    优化主页帮助按钮

--- .vitepress/theme/components/RightFloatButton.vue
+++ .vitepress/theme/components/RightFloatButton.vue
@@ -1,74 +1,89 @@
 <script setup>
-import { QuestionCircleOutlined, QqOutlined, WechatOutlined, BellOutlined } from '@ant-design/icons-vue';
-
+import {
+  QuestionCircleOutlined,
+  QqOutlined,
+  WechatOutlined,
+  BellOutlined,
+} from "@ant-design/icons-vue";
 </script>
 
 <template>
-    <div class="container">
-        <a-float-button-group shape="circle" :style="{ right: '15px' }">
-            <a-popover placement="left">
-                <template #content>
-                    <div>
-                        <img class="communication" src="/qq.svg" alt="qq群">
-                    </div>
-                </template>
-                <a-float-button>
-                    <template #icon>
-                        <WechatOutlined />
-                    </template>
-                </a-float-button>
-            </a-popover>
+  <div class="container">
+    <a-float-button-group
+      trigger="hover"
+      :style="{ right: '15px' }"
+    >
+      <template #icon>
+        <QuestionCircleOutlined />
+      </template>
 
-            <a-popover placement="left">
-                <template #content>
-                    <div>
-                        <img class="communication" src="/qq.svg" alt="qq群">
-                    </div>
-                </template>
-                <a-float-button>
-                    <template #icon>
-                        <QqOutlined />
-                    </template>
-                </a-float-button>
-            </a-popover>
+      <a-popover placement="left">
+        <template #content>
+          <div>
+            <img class="communication" src="/qq.svg" alt="qq群" />
+          </div>
+        </template>
+        <a-float-button>
+          <template #icon>
+            <WechatOutlined />
+          </template>
+        </a-float-button>
+      </a-popover>
 
-            <a-popover placement="left">
-                <template #content>
-                    <div>
-                        <p>1. 2024年中旬，上线注册和登录功能；发布技术博客模块，开放评论功能</p>
-                        <p>2. 2024年下旬，发布集成Gemini API的智能问答系统模块</p>
-                        <p>3. 2024年底，发布交流社区模块</p>
-                    </div>
-                </template>
-                <template #title>
-                    <span>
-                        <a-badge-ribbon text="预告" color="green">
-                            <div class="tooltip-title">Future Plan</div>
-                        </a-badge-ribbon>
-                    </span>
-                </template>
-                <a-float-button>
-                    <template #icon>
-                        <BellOutlined />
-                    </template>
-                </a-float-button>
-            </a-popover>
+      <a-popover placement="left">
+        <template #content>
+          <div>
+            <img class="communication" src="/qq.svg" alt="qq群" />
+          </div>
+        </template>
+        <a-float-button>
+          <template #icon>
+            <QqOutlined />
+          </template>
+        </a-float-button>
+      </a-popover>
 
-            <a-back-top :visibility-height="0" />
-        </a-float-button-group>
-    </div>
+      <a-popover placement="left">
+        <template #content>
+          <div>
+            <p>
+              1. 2024年中旬，上线注册和登录功能；发布技术博客模块，开放评论功能
+            </p>
+            <p>2. 2024年下旬，发布集成Gemini API的智能问答系统模块</p>
+            <p>3. 2024年底，发布交流社区模块</p>
+          </div>
+        </template>
+        <template #title>
+          <span>
+            <a-badge-ribbon text="预告" color="green">
+              <div class="tooltip-title">Future Plan</div>
+            </a-badge-ribbon>
+          </span>
+        </template>
+        <a-float-button>
+          <template #icon>
+            <BellOutlined />
+          </template>
+        </a-float-button>
+      </a-popover>
+
+      <a-back-top :visibility-height="0" />
+    </a-float-button-group>
+
+    
+  </div>
 </template>
 
 <style scoped>
 .communication {
-    width: 200px;
+  width: 200px;
 }
 
 .tooltip-title {
-    font-size: 16px;
-    color: #000;
-    font-weight: 600;
-    padding-top: 10px;
-    margin-bottom: 10px;
+  font-size: 16px;
+  color: #000;
+  font-weight: 600;
+  padding-top: 10px;
+  margin-bottom: 10px;
 }
 </style>
