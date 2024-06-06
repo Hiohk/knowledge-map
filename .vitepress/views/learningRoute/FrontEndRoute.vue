ebe942e65f549fa98918071860d58a2931ae214e
hk <Xiaojin@1017>
2024-06-06 15:22:20 +0800

    添加思维导图

---
+++ .vitepress/views/learningRoute/FrontEndRoute.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import frontEndData from "../../routeData/frontEndData.mjs";
+import MindElixir from "mind-elixir";
+
+onMounted(() => {
+  initMindMap();
+});
+
+const initMindMap = () => {
+  let options = {
+    el: "#map", // or HTMLDivElement
+    direction: MindElixir.LEFT,
+    draggable: true, // default true
+    contextMenu: true, // default true
+    toolBar: true, // default true
+    nodeMenu: true, // default true
+    keypress: true, // default true
+    locale: "zh_CN", // [zh_CN,zh_TW,en,ja,pt,ru] waiting for PRs
+    overflowHidden: false, // default false
+    mainLinkStyle: 2, // [1,2] default 1
+    mouseSelectionButton: 0, // 0 for left button, 2 for right button, default 0
+    contextMenuOption: {
+      focus: true,
+      link: true,
+      extend: [
+        {
+          name: "编辑节点",
+          onclick: () => {
+            alert("暂时不可编辑");
+          },
+        },
+      ],
+    },
+    before: {
+      insertSibling(el, obj) {
+        return false;
+      },
+      async addChild(el, obj) {
+        await sleep();
+        return true;
+      },
+    },
+  };
+
+  let mind = new MindElixir(options);
+  mind.init(frontEndData);
+};
+</script>
+
+<template>
+  <div id="map"></div>
+</template>
+
+<style scoped>
+#map {
+  height: 500px;
+  width: 100%;
+}
+</style>
---
+++ backend/mysql/basic05.md
@@ -0,0 +1,16 @@
+---
+prev:
+  text: "运算符"
+  link: "/backend/mysql/basic04"
+next:
+  text: "多表查询"
+  link: "/backend/mysql/basic06"
+---
+
+# 第 05 章 排序与分页
+
+## 1. 排序与分页
+
+<a-back-top />
+
+<reading-progress-bar/>
