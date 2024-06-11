d1b304e79d9962ba61c2232128471d302266e2a8
hk <Xiaojin@1017>
2024-06-11 18:03:08 +0800

    添加谷歌分析

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -23,7 +23,7 @@ export default defineConfig({
     `window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'TAG_ID');`
+    gtag('config', 'G-9QSPNM0JWD');`
   ]],
   markdown: {
     math: true,
