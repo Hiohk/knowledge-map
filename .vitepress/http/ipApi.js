195b083d7df48682cdf5408adbf64edc8541f409
hk <Xiaojin@1017>
2024-07-15 14:53:18 +0800

    https

--- .vitepress/http/ipApi.js
+++ .vitepress/http/ipApi.js
@@ -1,21 +1,35 @@
 import axios from './axios';
 
+function getBaseURL() {
+  let urlStr = '';
+
+  if (process.env.NODE_ENV === 'development') {
+    urlStr = 'http';
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    urlStr = 'https';
+  }
+  return urlStr;
+}
+
+
 // 获取当前的IP信息
 export async function getCurrentIp() {
-    try {
-        const response = await axios.get('http://edns.ip-api.com/json');
-        return response;
-    } catch (error) {
-        throw new Error(error);
-    }
+  try {
+    const response = await axios.get(`${getBaseURL()}://edns.ip-api.com/json`);
+    return response;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 // 根据IP查询详细地址信息
 export async function getAddressByIp(ip) {
-    try {
-        const response = await axios.get(`http://ip-api.com/json/${ip}?lang=zh-CN`);
-        return response;
-    } catch (error) {
-        throw new Error(error);
-    }
+  try {
+    const response = await axios.get(`${getBaseURL()}://ip-api.com/json/${ip}?lang=zh-CN`);
+    return response;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
