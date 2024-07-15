537793548991320d2fd9559cb7ff37e8998d24fe
hk <hk_snnu2020@163.com>
2024-07-15 22:06:10 +0800

    socket.io

--- .vitepress/views/onlineUser/OnlineUser.vue
+++ .vitepress/views/onlineUser/OnlineUser.vue
@@ -84,11 +84,20 @@ let socket = null;
 
 onMounted(trackUser);
 
-const baseURL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:8080";
+const getBaseUrl = () => {
+  let baseURL = "http://localhost:8080";
+  if (process.env.NODE_ENV === "production") {
+    baseURL = "wss://knowledge-server-production.up.railway.app";
+  }
+  return baseURL;
+};
+
 
 // 监听页面路径变化
 onMounted(() => {
-  socket = io(baseURL);
+  socket = io(getBaseUrl(), {
+    path: "/socket.io/"
+  });
 
   currentPath.value = window.location.pathname;
   startTime = Date.now();
