c06accd22a6ca5506580a0aa04156b657051ab8e
hk <Xiaojin@1017>
2024-07-16 09:29:42 +0800

    port

--- .vitepress/views/onlineUser/OnlineUser.vue
+++ .vitepress/views/onlineUser/OnlineUser.vue
@@ -87,7 +87,7 @@ onMounted(trackUser);
 const getBaseUrl = () => {
   let baseURL = "http://localhost:8080";
   if (process.env.NODE_ENV === "production") {
-    baseURL = "https://knowledge-server-production.up.railway.app:8080";
+    baseURL = "https://knowledge-server-production.up.railway.app:3030";
   }
   return baseURL;
 };
