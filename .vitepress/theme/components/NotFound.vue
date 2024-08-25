04e0419531d758afbefccb4bcbe2e1b3bcd71a3f
hk <hk_snnu2020@163.com>
2024-08-25 14:19:33 +0800

    修改not found展示内容

--- .vitepress/theme/components/NotFound.vue
+++ .vitepress/theme/components/NotFound.vue
@@ -1,9 +1,14 @@
 <script setup>
+import NotFoundLottie from "../../views/lottie/NotFoundLottie.vue";
+
 </script>
 
 <template>
     <div>
-        <a-result status="404" title="404" sub-title="Sorry, the page you visited does not exist.">
+        <a-result title="404" sub-title="对不起，您访问的页面不存在。">
+            <template #icon>
+                <not-found-lottie />
+            </template>
             <template #extra>
                 <a-button type="link" href="/knowledge-map/" target="_self">返回首页</a-button>
             </template>
---
+++ .vitepress/views/lottie/NotFoundLottie.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onMounted } from 'vue';
+import lottie from 'lottie-web';
+import animationData from "../../commonData/lottie/404.json";
+
+onMounted(() => {
+    lottie.loadAnimation({
+        container: document.getElementById('lottie-404'), // 动画将要渲染的DOM元素  
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        animationData: animationData
+    });
+});
+
+</script>
+
+<template>
+    <div>
+        <div id="lottie-404"></div>
+    </div>
+</template>
+
+<style scoped>
+#lottie-404 {
+    height: 350px;
+}
+</style>
